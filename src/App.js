04882c0f4e99b71e880db09b3c351f1d38158f53
Ramash3828 <ramash3828@gmail.com>
2022-07-01 01:08:28 +0600

    responsive navbar complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Components/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import CompleteTask from "./Components/CompleteTask.js/CompleteTask";
+import ToDos from "./Components/ToDos.js/ToDos";
+import Calender from "./Components/Calender/Calender";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/complete-task" element={<CompleteTask />} />
+                <Route path="/calender" element={<Calender />} />
+                <Route path="/to-dos" element={<ToDos />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/ToDos.js/ToDos.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ToDos = () => {
+    return (
+        <div>
+            <h2>To-Dos Page</h2>
+        </div>
+    );
+};
+
+export default ToDos;
